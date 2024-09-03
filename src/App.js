37f7485e5ba491afb4fb54a6ff9f5e683628a9ad
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-09-03 18:13:19 +0530

    revisiting app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 import { ethers } from "ethers";
-import MyEpicNFT from './utils/MyEpicNFT.json';
+import MyEpicNFT from "./utils/MyEpicNFT.json";
 
-const TWITTER_HANDLE = 'flashpunch_';
+const TWITTER_HANDLE = "flashpunch_";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // const OPENSEA_LINK = '';
 // const TOTAL_MINT_COUNT = 50;
@@ -22,10 +22,10 @@ const App = () => {
       console.log("We have the ethereum object", ethereum);
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    //  CHECKING IF CONNECTED TO GORELI 
-    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    //  CHECKING IF CONNECTED TO GORELI
+    let chainId = await ethereum.request({ method: "eth_chainId" });
     console.log("Connected to chain " + chainId);
 
     // String, hex code of the chainId of the Goerli test network
@@ -34,23 +34,21 @@ const App = () => {
       alert("You are not connected to the Goerli Test Network!");
     }
 
-
     if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
       // Setup listener! This is for the case where a user comes to our site
       // and ALREADY had their wallet connected + authorized.
-      setupEventListener()
-
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
-  }
+  };
 
   /*
-  * Implement your connectWallet method here
-  */
+   * Implement your connectWallet method here
+   */
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -61,14 +59,16 @@ const App = () => {
       }
 
       /*
-      * Fancy method to request access to account.
-      */
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
       // cheking if on goreli
 
-      //  CHECKING IF CONNECTED TO GORELI 
-      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      //  CHECKING IF CONNECTED TO GORELI
+      let chainId = await ethereum.request({ method: "eth_chainId" });
       console.log("Connected to chain " + chainId);
 
       // String, hex code of the chainId of the Goerli test network
@@ -78,23 +78,24 @@ const App = () => {
       }
 
       /*
-      * Boom! This should print out public address once we authorize Metamask.
-      */
+       * Boom! This should print out public address once we authorize Metamask.
+       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
       // Setup listener! This is for the case where a user comes to our site
       // and connected their wallet for the first time.
-      setupEventListener()
-
-
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
   );
@@ -111,28 +112,30 @@ const App = () => {
         // Same stuff again
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, MyEpicNFT.abi, signer);
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          MyEpicNFT.abi,
+          signer
+        );
 
         // THIS IS THE MAGIC SAUCE.
         // This will essentially "capture" our event when our contract throws it.
         // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
         });
 
-        console.log("Setup event listener!")
-
+        console.log("Setup event listener!");
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
-
-
+  };
 
   // mint contract
   const askContractToMintNft = async () => {
@@ -144,32 +147,36 @@ const App = () => {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, MyEpicNFT.abi, signer);
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          MyEpicNFT.abi,
+          signer
+        );
 
-        console.log("Going to pop wallet now to pay gas...")
+        console.log("Going to pop wallet now to pay gas...");
         let nftTxn = await connectedContract.makeAnEpicNFT();
 
-        console.log("Mining...please wait.")
+        console.log("Mining...please wait.");
         await nftTxn.wait();
 
-        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
-
+        console.log(
+          `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        );
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
-  }, [])
+  }, []);
 
   /*
-  * Added a conditional render! We don't want to show Connect to Wallet if we're already connected :).
-  */
+   * Added a conditional render! We don't want to show Connect to Wallet if we're already connected :).
+   */
   return (
     <div className="App">
       <div className="container">
@@ -181,7 +188,10 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+            <button
+              onClick={askContractToMintNft}
+              className="cta-button connect-wallet-button"
+            >
               Mint NFT
             </button>
           )}
